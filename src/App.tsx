3410a50bf4cd53a13cc080266ae51c5aa4f9d181
David Oluyale <oluyaled@gmail.com>
2022-10-27 11:02:35 +0100

    header and inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+const App: React.FC = (props) => {
   return (
     <div className="App">
-     
+      <header className="text-gray-600 body-font shadow">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span className="ml-3 text-xl">Openedge Crypto</span>
+          </a>
+        </div>
+      </header>
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">{`Crypto Tokens Manager`}</h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">{`Manage your Crypto Seamlessy on a single dashboard`}</p>
+          </div>
+          <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+            <div className="relative flex-grow w-full">
+              <label className="leading-7 text-sm text-gray-600">{`Private Key`}</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">{`Connect`}</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
-}
-
+};
 export default App;
